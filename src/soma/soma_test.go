95826ece71914793a9c262d6f449faa1980c0328
sombra255 <fabricio.tidir@gmail.com>
2020-09-20 13:03:37 -0300

    fix test

--- src/soma/soma_test.go
+++ src/soma/soma_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 	resultado := soma(5, 5)
-	esperado := 11
+	esperado := 10
 
 	if resultado != esperado {
 		t.Errorf("resultado '%v', esperado '%v'", resultado, esperado)
